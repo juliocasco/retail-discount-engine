618e48a9d18a26a3d8b5c7c5f114a93664f3ddc0
Julio Cascoe <Juliocasco@usf.edu>
2026-02-15 22:33:16 -0500

    I added Step 3 - I changed categories to some Sports categories to be more creative hope this does not create any issues with homeworks.

--- cc_4.js
+++ cc_4.js
@@ -1,5 +1,5 @@
 // Step 2 - Array 5 products//
-const products = [
+let products = [
     { name: "Rays Jersey ", category: "Baseball", price: 100, Inventory: 50 },
     { name: "Bucs Cap ", category: "Football", price: 50, Inventory: 100 },
     { name: "Yankees Jersey ", category: "Baseball", price: 150, Inventory: 30 },
@@ -7,3 +7,27 @@ const products = [
     { name: "Callaway HeadCover ", category: "Golf", price: 120, Inventory: 40 }
 ];
 
+// Step 3 - Apply category discounts//
+console.log("Discounts Items");
+for (let product of products) {
+    let discount = 0;
+
+    switch (product.category) {
+        case "Baseball":
+            discount = 0.20; // 20% discount
+            break;
+        case "Football":
+            discount = 0.15; // 15% discount
+            break;
+        case "Soccer":
+            discount = 0.10; // 10% discount
+            break;
+        case "Golf":
+            discount = 0.25; // 25% discount
+            break;
+            default:
+                discount = 0; // No Discount
+    }
+    product.discountedPrice = product.price * (1 - discount);
+
+    // Step 4 - Additional customer Type //
