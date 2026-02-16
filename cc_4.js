d949b0b9faf5e7ff89e399f8de1c205d988c63c0
Julio Cascoe <Juliocasco@usf.edu>
2026-02-15 22:25:27 -0500

    Step 2 Completed

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+// Step 2 - Array 5 products//
+const products = [
+    { name: "Rays Jersey ", category: "Baseball", price: 100, Inventory: 50 },
+    { name: "Bucs Cap ", category: "Football", price: 50, Inventory: 100 },
+    { name: "Yankees Jersey ", category: "Baseball", price: 150, Inventory: 30 },
+    { name: "Argentina Hoodie ", category: "Soccer", price: 80, Inventory: 20 },
+    { name: "Callaway HeadCover ", category: "Golf", price: 120, Inventory: 40 }
+];
+
